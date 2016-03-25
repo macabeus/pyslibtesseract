ecf21bf6c21f4be522244a07807672267e8c50bb
Bruno Macabeus <bruno.macabeus@gmail.com>
2016-03-25 14:39:10 -0300

    Fixbug: Now, the cmake runs during the installation of the package

--- setup.py
+++ setup.py
@@ -1,14 +1,46 @@
+from setuptools.command.install import install as InstallCommand
 from setuptools import setup
 
 
+class MyInstall(InstallCommand):
+    def run(self):
+        # Compile C code at src/cppcode
+        from subprocess import Popen, PIPE
+        import os
+
+        my_path = os.path.dirname(os.path.realpath(__file__))
+
+        print('Runing cmake at', my_path + '/src/cppcode/')
+        p = Popen(['cmake', '.'], stdout=PIPE, cwd=my_path + '/src/cppcode/')
+        print(p.stdout.read())
+        assert(p.wait() == 0)
+
+        if not os.path.exists(my_path + '/src/cppcode/Makefile'):
+            raise RuntimeError('Makefile was not generated!')
+
+        print('Runing make', my_path + '/src/cppcode/')
+        p = Popen(['make'], stdout=PIPE, cwd=my_path + '/src/cppcode/')
+        print(p.stdout.read())
+        assert(p.wait() == 0)
+
+        if not os.path.exists(my_path + '/src/cppcode/libpyslibtesseract.so'):
+            raise RuntimeError('pyslibtesseract.so was not generated!')
+
+        # Run install default
+        return InstallCommand.run(self)
+
+# Python setup
 setup(
     name='pyslibtesseract',
-    version='0.0.7',
+    version='0.0.15',
     author='Bruno Macabeus',
     description=('Integration of Tesseract for Python using a shared library'),
     keywords='python-tesseract OCR Python',
     url='https://github.com/brunomacabeusbr/pyslibtesseract',
     packages=['pyslibtesseract'],
     package_dir={'pyslibtesseract': 'src'},
-    package_data={'pyslibtesseract': ['cppcode/*.so']},
+    cmdclass={
+        'install': MyInstall
+    },
+    package_data={'pyslibtesseract': ['cppcode/main.cpp', 'cppcode/CMakeLists.txt', 'cppcode/libpyslibtesseract.so']},
 )
